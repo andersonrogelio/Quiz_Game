df4b0a724384950d0ad36ae09099b09935698eec
Anderson López <andersonrogelio0@gmail.com>
2022-04-25 15:13:15 -0300

    agregue estilos a los que es la priimer pagina

--- js/juego.js
+++ js/juego.js
@@ -1,9 +1,14 @@
 var preguntas_cat_actual;
 var arreglo_preguntas;
 var index_pregunta;
-var puntaje = 0;
-var nivel_juego = 0;
-var arreglo = [];
+var posicion_res = [1,2,3,4];
+var puntos = 0;
+var premio = {
+    "jugador":"",
+    "puntos":0
+};
+premio.jugador = localStorage.getItem("jugador");
+var nivel_juego = 1;
 //Esta funcion me generara un numero aleatorio entre un rango dado
 function numero_aleatorio(inicio,final){
     return inicio + Math.floor(Math.random()*final);
@@ -19,100 +24,87 @@ function cargar_botones(pregunta_actual) {
     index_pregunta = numero_aleatorio(1,5);
     document.getElementById("mostrar_pregunta").innerHTML = pregunta_actual.P;
     let html = "";
-    let pos = 0;
-    for (i=1;i<=4;i++){
-        if (i === index_pregunta){
-            html = html + `<button id="b_`+ i +`" value = "`+ pregunta_actual.C +`">`+ pregunta_actual.C +`</button>
-            `
-        }else{
-            pos = pos + 1;            
-            switch (pos) {
-                case 1:
-                    html = html + `<button id="b_`+ i +`" value = "`+ pregunta_actual.I1 +`">`+ pregunta_actual.I1 +`</button>
-                    `
-                    break;
-               case 2:
-                html = html + `<button id="b_`+ i +`" value = "`+ pregunta_actual.I2 +`">`+ pregunta_actual.I2 +`</button>
-                `
-                   break;
-               case 3:
-                html = html + `<button id="b_`+ i +`" value = "`+ pregunta_actual.I3 +`">`+ pregunta_actual.I3 +`</button>
-                `
-                   break;
-            
-                default:
-                    break;
-            };
-        };
-    };
-    document.getElementById("botones").innerHTML = html;
+    console.log(posicion_res);
+    posicion_res.sort(() => (Math.random()-0.5));
+    console.log(posicion_res);
+    html = html + `
+    <button id="b_`+ posicion_res[0] +`" value = "`+ pregunta_actual.C +`">`+ pregunta_actual.C +`</button>
+    <button id="b_`+ posicion_res[1] +`" value = "`+ pregunta_actual.I1 +`">`+ pregunta_actual.I1 +`</button>
+    <button id="b_`+ posicion_res[2] +`" value = "`+ pregunta_actual.I2 +`">`+ pregunta_actual.I2 +`</button>
+    <button id="b_`+ posicion_res[3] +`" value = "`+ pregunta_actual.I3 +`">`+ pregunta_actual.I3 +`</button>
+    `
+    document.getElementById("botones").innerHTML = html;  
     
 };
 function cargar_pregunta(nivel){
-    switch (nivel) {
-        case 1:
-            index_pregunta = numero_aleatorio(1,5);
+    if (nivel === 1) {
+            index_pregunta = numero_aleatorio(0,4);
+            console.log(index_pregunta);
             preguntas_cat_actual = JSON.parse(arreglo_preguntas[index_pregunta][0]);
             cargar_botones(preguntas_cat_actual);
-            break;
-        case 2:
-            index_pregunta = numero_aleatorio(6,10);
-            preguntas_cat_actual = JSON.parse(arreglo_preguntas[index_pregunta][0]);
-            cargar_botones(preguntas_cat_actual);
-            break;
-        case 3:
-            index_pregunta = numero_aleatorio(11,15);
-            preguntas_cat_actual = JSON.parse(arreglo_preguntas[index_pregunta][0]);
-            cargar_botones(preguntas_cat_actual);
-            break;
-        case 4:
-            index_pregunta = numero_aleatorio(15,20);
-            preguntas_cat_actual = JSON.parse(arreglo_preguntas[index_pregunta][0]);
-            cargar_botones(preguntas_cat_actual);
-            break;
-          case 5:
-            index_pregunta = numero_aleatorio(20,25);
-            preguntas_cat_actual = JSON.parse(arreglo_preguntas[index_pregunta][0]);
-            cargar_botones(preguntas_cat_actual);
-            break;
-    
-        default:
-            break;
     }
-    // preguntas_cat_actual = arreglo_preguntas[nivel];
-    // console.log(arreglo_preguntas[nivel]);
-    // console.log("esta es la preguntas categoria actual");
-    // console.log(preguntas_cat_actual);
-    // preguntas_cat_actual = preguntas[nivel];
-    // nivel_juego = nivel_juego + 1;
-    // pregunta_actual = preguntas_cat_actual[index_pregunta];
-    // pregunta_actual = JSON.parse(preguntas[nivel]);
-    // console.log(pregunta_actual);
+    if (nivel === 2) {
+            index_pregunta = numero_aleatorio(5,9);
+            preguntas_cat_actual = JSON.parse(arreglo_preguntas[index_pregunta][0]);
+            cargar_botones(preguntas_cat_actual);
+    }
+    if (nivel === 3) {
+          index_pregunta = numero_aleatorio(10,14);
+            preguntas_cat_actual = JSON.parse(arreglo_preguntas[index_pregunta][0]);
+            cargar_botones(preguntas_cat_actual);
+    }
+    if (nivel === 4) {
+                index_pregunta = numero_aleatorio(15,19);
+            preguntas_cat_actual = JSON.parse(arreglo_preguntas[index_pregunta][0]);
+            cargar_botones(preguntas_cat_actual);
+    }
+    if (nivel === 5) {
+                    index_pregunta = numero_aleatorio(20,24);
+            preguntas_cat_actual = JSON.parse(arreglo_preguntas[index_pregunta][0]);
+            cargar_botones(preguntas_cat_actual);
+    }
+
 }
 function verificar_respuesta(identificador){
-    if ((document.getElementById(identificador).value === pregunta_actual.C)){
-        cargar_pregunta(nivel_juego +1);
-        switch (nivel_juego) {
-            case 1:
-                puntaje = puntaje + 10
-                break;
-             case 2:
-                puntaje = puntaje + 15;
-                break;
-             case 3:
-                puntaje = puntaje + 20;
-                 break;
-             case 4:
-                 puntaje = puntaje + 25;
-                 break;
-             case 5:
-                 puntaje = puntaje + 30;
-                 break;
-            default:
-                break;
-        };
+    if ((document.getElementById(identificador).value === preguntas_cat_actual.C)){
+        if (nivel_juego === 1) {
+            puntos =+ 10;
+            premio.puntos = puntos;
+        }
+        if (nivel_juego === 2) {
+            puntos =+ 15;
+            premio.puntos = puntos;
+        }
+        if (nivel_juego === 3) {
+            puntos =+ 20;
+            premio.puntos = puntos;
+        }
+        if (nivel_juego === 4) {
+            puntos =+ 25;
+            premio.puntos = puntos;
+        }
+        if (nivel_juego === 5) {
+            puntos =+ 30;
+            premio.puntos = puntos;
+        }
+        nivel_juego = nivel_juego + 1;
+        cargar_pregunta(nivel_juego);
     }else{
-        localStorage.setItem("puntos",toString(puntaje));
+        console.log(premio);
+        localStorage.setItem("puntaje",JSON.stringify(premio));
+        window.location.href = "pantalla_inicial.html";
     };
 };
-cargar_pregunta(1);
+cargar_pregunta(nivel_juego);
+document.getElementById("b_1").addEventListener("click",function(e){
+    verificar_respuesta("b_1");
+});
+document.getElementById("b_2").addEventListener("click",function(e){
+    verificar_respuesta("b_2");
+});
+document.getElementById("b_3").addEventListener("click",function(e){
+    verificar_respuesta("b_3");
+});
+document.getElementById("b_4").addEventListener("click",function(e){
+    verificar_respuesta("b_4");
+});
